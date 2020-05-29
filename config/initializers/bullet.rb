f4074597cd15c8bd981dc854d119feac2c2a71ad
Andrew Mason <andrewmcodes@protonmail.com>
2020-05-29 02:16:02 -0400

    chore: :wrench: Updates to some tooling configuration (#1)
    Adds 'standard-version' for automated changelogs, add configuration files, and document the tooling

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,11 @@
+if defined? Bullet
+  Bullet.enable = true
+  Bullet.sentry = false
+  Bullet.alert = false
+  Bullet.bullet_logger = true
+  Bullet.console = false
+  Bullet.rails_logger = true
+  Bullet.honeybadger = false
+  Bullet.add_footer = false
+  Bullet.skip_html_injection = false
+end
