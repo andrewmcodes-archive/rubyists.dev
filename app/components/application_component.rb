65442511b9af242a14d00520e34e9021466add92
andrewmcodes <andrewmcodes@protonmail.com>
2020-05-29 05:51:47 -0400

    feat: :art: Move flash to component

---
+++ app/components/application_component.rb
@@ -0,0 +1,13 @@
+class ApplicationComponent < ViewComponent::Base
+
+  private
+
+  def find_or_fallback(supplied, valid, fallback)
+    if valid.include?(supplied)
+      supplied
+    else
+      raise ArgumentError if Rails.env.development?
+      fallback
+    end
+  end
+end
