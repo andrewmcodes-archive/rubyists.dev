b983641f2666f9b4d1473c2940202e8596afd923
andrewmcodes <andrewmcodes@protonmail.com>
2020-05-30 06:20:45 -0400

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,16 +2,10 @@
 
 # Sidekiq::Extensions.enable_delay!
 
-defaults = {
-  url: ENV["REDIS_URL"],
-  db: ENV["REDIS_DB"],
-  namespace: ENV["SIDEKIQ_NAMESPACE"]
-}
-
 Sidekiq.configure_server do |config|
-  config.redis = defaults.merge(size: 50)
+  config.redis = {url: ENV["REDIS_URL"], size: ENV.fetch("REDIS_MAX_THREADS", 5).to_i}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = defaults.merge(size: 10)
+  config.redis = {url: ENV["REDIS_URL"], size: ENV.fetch("REDIS_MAX_THREADS", 5).to_i}
 end
